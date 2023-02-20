aa58c050cb03274c41f3dbc30fc50c9370309434
leofss <leofernandess11@gmail.com>
2023-02-20 10:54:26 -0300

    Add: firebase setup

---
+++ src/config/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBrQqb75KbJjC4b5nzO7kjdafF2HU_CFIc",
+    authDomain: "dojosite-d1001.firebaseapp.com",
+    projectId: "dojosite-d1001",
+    storageBucket: "dojosite-d1001.appspot.com",
+    messagingSenderId: "905619389591",
+    appId: "1:905619389591:web:8a987c37fabe73e6b93dd2"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore();
+
+const projectAuth = firebase.auth()
+
+const timestamp = firebase.firestore.Timestamp
+
+export {projectFirestore, projectFirestore, timestamp}
