46efcefcd5fa25c2953b9fe431b52c7f7c245b19
leofss <leofernandess11@gmail.com>
2023-02-21 09:41:01 -0300

    Add: setting up routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
+import Create from './pages/create/Create';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Project from './pages/project/Project';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <div className='container'>
+          <Switch>
+            <Route exact path="/">
+              <Dashboard/>
+            </Route>
+          </Switch>
 
+          <Switch>
+            <Route path="/create">
+              <Create/>
+            </Route>
+          </Switch>
+
+          <Switch>
+            <Route path="/projects/:id">
+              <Project/>
+            </Route>
+          </Switch>
+
+          <Switch>
+            <Route path="/login">
+              <Login/>
+            </Route>
+          </Switch>
+
+          <Switch>
+            <Route path="/signup">
+              <Signup/>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
